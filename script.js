7b6070f6a2998d2388542c56c935c23b0229b53b
DIVIJA R L <151992943+R-L-Divija@users.noreply.github.com>
2024-09-08 21:20:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// Developer --> Adarsh Maurya
+
+console.log('I am working! ');
+
+const author = 'Divija';
+
+console.log(`Developer ${author}`);
+
+var typed = new Typed('.dynamic-txt', {
+  strings: ['Web Developer!', 'Programmer!', 'Blogger!'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+let menuBtnCond = true;
+let checkLabel = document.getElementById('check-label');
+
+const menuBtnChanger = function () {
+  if (menuBtnCond) {
+    checkLabel.innerHTML = `<i class='bx bx-x'></i>`;
+    menuBtnCond = false;
+  } else {
+    checkLabel.innerHTML = `<i class='bx bx-menu'></i>`;
+
+    menuBtnCond = true;
+  }
+};
+
+let menuBtn = document.getElementById('check');
+menuBtn.addEventListener('click', menuBtnChanger);
